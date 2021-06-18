b100d6049add41b1707443d59fcb6867c4072b04
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-06-19 02:34:21 +0530

    Last 4 sections created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    // colors:{
+    //   primary: "#FF6464",
+    //   secondary:{
+    //     50:"#ffab45",
+    //     100:"#ffab55",
+    //     150:"#ffab75",
+    //     250:"#ffabf5",
+    //   },
+    //   accent: colors.cyan,
+    //   //Here we are including grey default colours from tailwind into our project
+    //   gray: colors.gray,
+    //   blue: colors.indigo  
+    // },
+    // Whatever we write inside extend then their new classes are getting added and default classes are also not ignored/removed
+    extend: {
+      padding:{
+        0.75: ".1875rem",
+      },
+      colors:{
+        magenta:{
+          lighter:"#12346",
+          noraml:"#ff85fa",
+          darker:"#abc566"
+        },
+        background1: "#f3f1e7", 
+        maroon: "#FF1122" 
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      padding: ["hover"],
+    },
   },
   plugins: [],
 }
